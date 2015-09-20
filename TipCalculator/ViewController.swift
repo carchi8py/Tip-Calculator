a1895fda3f85b514b8d684f2f47aa6ee87309f68
Chris Archibald <carchi8py@gmail.com>
2015-09-19 21:06:12 -0700

    Set up the constraints.

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var amountTextField: UITextField!
+    @IBOutlet weak var tip10Label: UILabel!
+    @IBOutlet weak var tip15Label: UILabel!
+    @IBOutlet weak var tip20Label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func calculateTipButtonPressed(sender: AnyObject) {
+        println("tip")
+    }
 
 }
 
