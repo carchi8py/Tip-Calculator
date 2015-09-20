3b0af5071cfd1c84febccfb6a660e82339c85eb2
Chris Archibald <carchi8py@gmail.com>
2015-09-19 22:19:10 -0700

    Added total amount to pay.

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -12,11 +12,17 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var amountTextField: UITextField!
     @IBOutlet weak var customTipTextField: UITextField!
+    @IBOutlet weak var numberOfPeopleTextField: UITextField!
     @IBOutlet weak var tip10Label: UILabel!
     @IBOutlet weak var tip15Label: UILabel!
     @IBOutlet weak var tip20Label: UILabel!
     @IBOutlet weak var tipCustomLabel: UILabel!
     @IBOutlet weak var customTipPrecentage: UILabel!
+    @IBOutlet weak var howMuch10Label: UILabel!
+    @IBOutlet weak var howMuch15Label: UILabel!
+    @IBOutlet weak var howMuch20Label: UILabel!
+    @IBOutlet weak var howMuchCustomLabel: UILabel!
+    @IBOutlet weak var customTipPrecentage2: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -48,13 +54,35 @@ class ViewController: UIViewController {
         tip15Label.text = numberFormatter.stringFromNumber(tip15)
         tip20Label.text = numberFormatter.stringFromNumber(tip20)
         
+        if numberOfPeopleTextField.text.isEmpty {
+            howMuch10Label.text = tip10Label.text
+            howMuch15Label.text = tip15Label.text
+            howMuch20Label.text = tip20Label.text
+        } else {
+            var numberPeople = (numberOfPeopleTextField.text as NSString).doubleValue
+            howMuch10Label.text = numberFormatter.stringFromNumber((amount + tip10) / numberPeople)
+            howMuch15Label.text = numberFormatter.stringFromNumber((amount + tip15) / numberPeople)
+            howMuch20Label.text = numberFormatter.stringFromNumber((amount + tip20) / numberPeople)
+        }
+        
         if customTipTextField.text.isEmpty {
             tipCustomLabel.text = ""
             customTipPrecentage.text = ""
+            howMuchCustomLabel.text = ""
+            customTipPrecentage2.text = ""
+            
         } else {
             var tipCustom = amount * ((customTipTextField.text as NSString).doubleValue / 100)
             tipCustomLabel.text = numberFormatter.stringFromNumber(tipCustom)
             customTipPrecentage.text = customTipTextField.text + "%"
+            customTipPrecentage2.text = customTipPrecentage.text
+            if numberOfPeopleTextField.text.isEmpty
+            {
+                howMuchCustomLabel.text = ""
+            } else {
+                var numberPeople = (numberOfPeopleTextField.text as NSString).doubleValue
+                howMuchCustomLabel.text = numberFormatter.stringFromNumber((amount + tipCustom) / numberPeople)
+            }
         }
     }
     
