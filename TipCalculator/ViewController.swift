0ecee207cd2b9842ef9cc5644e6733224fffe8bd
Chris Archibald <carchi8py@gmail.com>
2015-09-19 21:29:05 -0700

    On load show data.

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -18,6 +18,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        calculateTipButtonPressed(self)
     }
 
     override func didReceiveMemoryWarning() {
